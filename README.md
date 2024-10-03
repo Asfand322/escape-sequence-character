fc5214b29cd760c0f7a8c927e93d2302c8de9b2d
Asfand322 <asfandsheikh321@gmail.com>
2024-10-03 20:30:29 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# escape-sequence-character
+ 
---
+++ index.js
@@ -0,0 +1,19 @@
+// escape sequence character
+
+let fruits = 'bana\'na'
+console.log(fruits)
+
+// string in string
+// result will show bana'na
+// because of \
+
+let fruit = 'bana\nna'
+console.log(fruit)
+
+// result will be bana
+// na
+// na will be on second line coz of \n
+// other escaping characters are \t for tab and \r for carriage return
+
+let fruit1 = 'bana\tna'
+console.log(fruit1)
